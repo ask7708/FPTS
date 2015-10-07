032ae1ad2159f098a0f0315b2b0fc03f2fcee4e8
ask7708 <ask7708@rit.edu>
2015-10-07 15:07:39 -0400

    added getter methods to the Equity class

--- src/Release1/Equity.java
+++ src/Release1/Equity.java
@@ -9,12 +9,33 @@ package Release1;
  */
 public abstract class Equity {
 	
+	/**
+	 * 
+	 */
 	private String name;
+
+	/**
+	 * 
+	 */
 	private String tickSymbol;
+	
+	/**
+	 * 
+	 */
 	private double marketIndex;
+	
+	/**
+	 * 
+	 */
 	private String industrySec;
 	
 	
+	/**
+	 * 
+	 * 
+	 * @param name
+	 * @param tickSymbol
+	 */
 	public Equity(String name, String tickSymbol){
 		
 		this.name = name;
@@ -24,9 +45,44 @@ public abstract class Equity {
 		
 	}
 
+	/**
+	 * 
+	 * 
+	 * @return name
+	 */
+	public String getName(){
+		
+		return this.name;
+	}
 	
+	/**
+	 * 
+	 * 
+	 * @return tickSymbol
+	 */
+	public String getTickSymbol(){
+		
+		return this.tickSymbol;
+	}
 	
+	/**
+	 * 
+	 * 
+	 * @return marketIndex
+	 */
+	public double getMakrketIndex(){
+		
+		return this.marketIndex;
+	}
 	
-	
-	
+	/**
+	 * 
+	 * 
+	 * @return industrySec
+	 */
+	public String getIndustrySec(){
+		
+		
+		return this.industrySec;
+	}
 }
