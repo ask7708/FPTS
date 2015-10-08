344bf1661b3a4d4098745616b65734ba2a87616d
ask7708 <ask7708@rit.edu>
2015-10-08 19:25:31 -0400

    Adding BankAccount and MarketAccount classes

---
+++ src/Release1/MarketAccount.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package Release1;
+
+import java.time.LocalDate;
+
+/**
+ * @author Arsh
+ *
+ */
+public class MarketAccount extends Account {
+
+	public MarketAccount(String accountName, int amount, LocalDate date) {
+		super(accountName, amount, date);
+		// TODO Auto-generated constructor stub
+	}
+
+}
