6179de788264c509079a76b5e74e2f4401565e15
Talal <talal.nks@gmail.com>
2015-10-08 15:53:21 -0400

    ADDING DASHBOARD

--- src/Release1/DashboardView.java
+++ src/Release1/DashboardView.java
@@ -1,5 +1,13 @@
 package Release1;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
 public class DashboardView extends View {
    
    /**
@@ -9,7 +17,41 @@ public class DashboardView extends View {
    public DashboardView(Portfolio portfolio) {
       
       super();
-      this.screen.setTitle("FPTS - " + portfolio.getUsername() + " - Home");
-   }
+      
+      this.screen.setTitle("FPTS - "+ portfolio.getUsername() +  " - Home");
+      
+      JPanel top = new JPanel();
+  
+      JLabel helloText = new JLabel("Hello");
+      JLabel firstNameFaild = new JLabel(portfolio.getUserFName());
+      JLabel lastNameFalid = new JLabel(portfolio.getUserLName());
+      //helloText.setHorizontalAlignment(10);;
+      helloText.setBounds(30,40,280,30);
+      top.add(helloText);
+      top.add(lastNameFalid, BorderLayout.WEST);
+      
 
+      
+      
+      /**
+      JPanel center = new JPanel();
+      JLabel helloTex = new JLabel("Hello");
+      center.add(helloTex);
+      **/
+      
+      this.screen.add(top); 
+     // this.screen.add(center);
+      screen.setSize(400, 600);
+
+   }
+   public static void main(String[] args) {
+	   //creat a port
+	     User user = new User("itnks", "nasser");
+	     user.setFirstName("Talal");
+	     user.setLastName("Alsarrani");
+	     Portfolio po = new Portfolio(user);
+	     DashboardView dash= new DashboardView(po);
+	     dash.show();
+	   
+   }
 }
