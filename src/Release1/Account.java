a2d91e7645292ca15f0820d361abe894efaa10c2
ask7708 <ask7708@rit.edu>
2015-10-08 19:18:03 -0400

    Added the Account abstract class for use with subclasses (Bank and Market)

---
+++ src/Release1/Account.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package Release1;
+import java.time.LocalDate;
+import java.util.Date;
+
+/**
+ * @author Arsh
+ *
+ */
+public abstract class Account {
+	
+	/**
+	 * The name of an account
+	 */
+	private String accountName;
+	
+	/**
+	 * The amount in the type of account
+	 */
+	private int amount;
+	
+	/**
+	 * The date the type of account was added
+	 */
+	private Date date;
+	
+	/**
+	 * Creates an account object with the name, initial amount, 
+	 * and the date it was added.
+	 * 
+	 * @param accountName
+	 * @param amount
+	 * @param date
+	 */
+	public Account(String accountName, int amount, Date date){
+		
+		this.accountName = accountName;
+		this.amount = amount;
+		this.date = date;
+		
+	}
+
+	/**
+	 * Returns the account name of the 
+	 * specified account
+	 * 
+	 * @return accountName
+	 */
+	public String getAccountName() {
+		return accountName;
+	}
+
+	/**
+	 * Set the name of an account
+	 * 
+	 * @param accountName
+	 */
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	/**
+	 * Returns the amount in the type of account
+	 * 
+	 * @return amount
+	 */
+	public int getAmount() {
+		return amount;
+	}
+
+	/**
+	 * Sets the amount for the account
+	 * 
+	 * @param amount
+	 */
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	
+	
+	/* Testing some methods within account class
+	 * ***DO NOT ERASE***
+	public static void main(String args[]){
+		
+		
+		LocalDate randomDate = LocalDate.now();
+		System.out.println(randomDate.getMonth()+" "+randomDate.getDayOfMonth()+" "+randomDate.getYear());
+		
+		
+	}
+	*/
+}
