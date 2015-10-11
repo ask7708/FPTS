7f6e3a344cf6a29654d593c9768454776a09a666
Daniel Cypher <dcc7331@g.rit.edu>
2015-10-10 22:15:47 -0400

    Adding the parameters and get functions to transaction.

--- src/Release1/Transaction.java
+++ src/Release1/Transaction.java
@@ -2,11 +2,38 @@ package Release1;
 import java.util.Date;
 
 public abstract class Transaction {
-	String java;
 
 	/**
 	 * the date and time the transaction was made
 	 */
 	private Date time;
+	private int amount;
+	private Object reciever;
+	private Object transfer;
+	
+	public Transaction(Date time, int amount, Object reciever,Object transfer){
+		time = this.time;
+		amount = this.amount;
+		reciever = this.reciever;
+		transfer = this.transfer;
+		
+	}
+	
+	public Date getTime(){
+		return time;
+	}
+	
+	public int getAmount(){
+		return amount;
+	}
+	
+	public Object getReciever(){
+		return reciever;
+	}
+	
+	public Object getTransfer(){
+		return transfer;
+	}
+	
 
 }
