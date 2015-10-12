38d4eefaab89daf5d9e8d50ded6c8fc4ced0764f
ask7708 <ask7708@rit.edu>
2015-10-12 02:17:28 -0400

    Made rough method to read in users for verification

--- src/Release1/Login.java
+++ src/Release1/Login.java
@@ -1,8 +1,10 @@
 package Release1;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 
@@ -15,21 +17,32 @@ public class Login {
 
 
  
-        String name = "itnks,12345";
+        String name = "itnks";
         String pass = "12345";
         
         File f = new File("users.txt");
+       
+        String tempName = "";
+        String tempPass = "";
+        
         Scanner numScan = new Scanner(f);
+        numScan.useDelimiter(",");
          
         String line;
-         
-        while (numScan.hasNext())
-        {
-            line = numScan.next();
-            System.out.print(line);
-         
-            }
+        String[] temp;
+        
+        while (numScan.hasNextLine())
+		{
+        	line = numScan.nextLine();
+        	temp = line.split(",");
+        	if(temp[0].equals(name)){
+
+        			if(temp[1].equals(pass)){
+        			System.out.print(line+"\n");
+        			}
+        	}
+		   }
         }
 		
 
-}}
+}
