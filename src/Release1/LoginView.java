a57d612e0b7389695f5462e373c7c059b799894c
Talal <talal.nks@gmail.com>
2015-10-11 23:00:36 -0400

    added the view for login

--- src/Release1/LoginView.java
+++ src/Release1/LoginView.java
@@ -1,66 +1,101 @@
 package Release1;
 
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.SpringLayout;
 
-public class LoginView extends View {
+public class LoginView extends JFrame {
    
    /**
     * Creates the login screen that will first be displayed when the
     * application is first started
     */
+    private static final long serialVersionUID = 1L;
+    private JPanel cardPanel, loginPanel, registerPanel, mainPanel;
+    private JLabel dahboardLabel, registerLabel;
+    private JButton loginboardButton, registerButton;
+    private CardLayout cardLayout = new CardLayout();
    public LoginView() {
-      
-      screen.setTitle("FPTS - Login");
-      
-      JPanel top = new JPanel();
-      SpringLayout layout = new SpringLayout();
-      top.setLayout(layout);
-      
-      JTextPane acronym = new JTextPane();
-      acronym.setText("FPTS");
-      acronym.setOpaque(false);
-      acronym.setEditable(false);
-      top.add(acronym);
-      
-      layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, acronym, 0, SpringLayout.HORIZONTAL_CENTER, top);
-      
-      JTextPane fullName = new JTextPane();
-      fullName.setText("Financial Portfolio Tracking System");
-      fullName.setOpaque(false);
-      fullName.setEditable(false);
-      top.add(fullName);
-      
-      layout.putConstraint(SpringLayout.NORTH, fullName, 10, SpringLayout.SOUTH, acronym);
-      layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, fullName, 0, SpringLayout.HORIZONTAL_CENTER, top);
-      
-      JTextField username = new JTextField("Enter username here...");
-      top.add(username);
-      
-      JPasswordField pswdField = new JPasswordField();
-      pswdField.setEchoChar('*');
-      top.add(pswdField);
-      
-      layout.putConstraint(SpringLayout.NORTH, username, 50, SpringLayout.SOUTH, fullName);
-      layout.putConstraint(SpringLayout.NORTH, pswdField, 10, SpringLayout.SOUTH, username);
+	      
+	      cardPanel = new JPanel();
+	      mainPanel = new JPanel();
+	      cardPanel.setLayout(cardLayout);
+	      
+	      loginPanel = new JPanel();
+	      registerPanel = new JPanel();
+	    
+	      dahboardLabel = new JLabel("");
+	      registerLabel = new JLabel("Register");
 
-      layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, username, 0, SpringLayout.HORIZONTAL_CENTER, top);
-      layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, pswdField, 0, SpringLayout.HORIZONTAL_CENTER, top);
+	      
+	      loginPanel.add(dahboardLabel);
+	      registerPanel.add(registerLabel);
 
 
-      
-      
-      screen.add(top);
-      screen.setSize(400, 600);
+	      cardPanel.add(loginPanel, "1");
+	      cardPanel.add(registerPanel, "2");
+	      
+	      loginboardButton = new JButton("Login");
+	      
+	     
+ 
+			JLabel userLabel = new JLabel("User");
+			loginPanel.add(userLabel);
+
+			JTextField userText = new JTextField(20);
+			loginPanel.add(userText);
+
+			JLabel passwordLabel = new JLabel("Password");
+			loginPanel.add(passwordLabel);
+
+			JPasswordField passwordText = new JPasswordField(20);
+			loginPanel.add(passwordText);
+
+			JButton loginButton = new JButton("login");
+			loginPanel.add(loginButton);
+	      
+			loginboardButton.addActionListener(new ActionListener() {
+	    	  
+	          public void actionPerformed(ActionEvent e) {
+	              cardLayout.show(cardPanel, "1");
+	          }
+	      });
+	      
+	      
+	      
+	      
+	      
+	      registerButton = new JButton("Register");
+	      registerButton.addActionListener(new ActionListener() {
+
+	          public void actionPerformed(ActionEvent e) {
+	              cardLayout.show(cardPanel, "2");
+	          }
+	      });
+	      
+	      
+	      mainPanel.add(loginboardButton);
+	      mainPanel.add(registerButton);
+	      add(cardPanel);
+	      add(mainPanel, BorderLayout.SOUTH);
+	      setSize(600, 600);
    }
    
   public static void main(String[] args) {
    
      LoginView login = new LoginView();
-     login.show();
+     login.setVisible(true);
   }
   
 }
