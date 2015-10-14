489cc591f98a379bf78d598f9142f7e9c3ed5c5d
Daniel Cypher <dcc7331@g.rit.edu>
2015-10-13 23:58:06 -0400

    The button for Transaction View haven't tested on a jpanel yet

---
+++ src/Release1/TransactionView.java
@@ -0,0 +1,43 @@
+package Release1;
+
+import java.awt.Button;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+public class TransactionView {
+	private Button yourButton;
+
+	public TransactionView(){
+		
+	}
+	
+	void textDisplay(File text){
+		yourButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+			try {
+			JTextArea ta = new JTextArea(20, 60);
+			ta.read(new FileReader(text), null);
+			ta.setEditable(false);
+			JOptionPane.showMessageDialog(yourButton, new JScrollPane(ta));
+			}
+			catch (IOException ioe) {
+			ioe.printStackTrace();
+			}
+			}
+			
+			public void main(String[] args){
+				
+				
+			}
+
+			
+			});
+	}
+}
