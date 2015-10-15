a3d61cb640f8569013018eaaaebb532ca6d16a69
Talal <talal.nks@gmail.com>
2015-10-15 19:46:03 -0400

    adding the routing number method

--- src/Release1/Account.java
+++ src/Release1/Account.java
@@ -3,8 +3,13 @@
  */
 package Release1;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.Scanner;
 
 /**
  * @author Arsh
@@ -123,23 +128,92 @@ public abstract class Account implements Holdings {
 		this.date = date;
 	}
 	
-	public String genretRoutingNumber(File file){
-		String rNumber = "";
-		
-		
-		
-		return rNumber; 
+	
+	   /**
+	    * this method takes an account file and update it with a unique routing number. 
+	    * @param user - gets the user file 
+	    * @throws FileNotFoundException
+	    * @throws UnsupportedEncodingException
+	    */
+	   public void getRoutingNumber(String user) throws FileNotFoundException, UnsupportedEncodingException {
+			   
+		   //
+		   
+	    	File data = new File("test.txt");
+			Scanner dataRead = null;
+			try {
+				dataRead = new Scanner(data);
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//PrintWriter writer = new PrintWriter(data, "UTF-8");
+			//writer.println("hello");
+			//String bank  ("!BANK","Test Bank of Testville","12345.67","123456789","987654321","20151013");
+			//writer.print("hello!");
+			String line;
+			String[] temp = null;
+			String rNumber = "";
+			while (dataRead.hasNextLine())
+			{
+	      	line = dataRead.nextLine();
+	      	line = line.replace("\"", "");
+	      	line = line.replace(", ", "");
+	      	temp = line.split(",");
+	      	//ReadHoldingsContext readAccount = new ReadHoldingsContext(new ReadAccountHolding());
+	      	
+	      	if(temp[0].equals("!BANK")){
+	      				System.out.println("this is good");
+	      				if (temp[4].length() != 9){
+	      				temp[4] = genretNumber();
+	      				System.out.println(Arrays.toString(temp));
+	      				
+	      				}
+	      	}
+	      		
+	      	}
+			PrintWriter writer = new PrintWriter(data, "UTF-8");
+			writer.println(Arrays.toString(temp));
+			writer.close();
+		   
+	}  
+	/**
+	 * validate the routing number.
+	 * @param string 
+	 * @return true if has 9 numbers. 
+	 */
+	public boolean validateRNumber(String string) {
+		if(string.length() != 9){
+			return false;
+		}else{
+			return true;
+		}	
 	}
+	/**
+	 * 
+	 * @return string that has a unique number in string data; 
+	 */
+	public static String genretNumber(){
+        long timeSeed = System.nanoTime(); 
+        double randSeed = Math.random() * 1000; 
+        long midSeed = (long) (timeSeed * randSeed); 
+        String s = midSeed + "";
+        String rNumber = s.substring(0, 9);
+		return rNumber;        
+	}
+
+	
 	
 	/* Testing some methods within account class
-	 * ***DO NOT ERASE***
+	 * ***DO NOT ERASE*
 	public static void main(String args[]){
 		
 		
 		LocalDate randomDate = LocalDate.now();
-		System.out.println(randomDate.getMonth()+" "+randomDate.getDayOfMonth()+" "+randomDate.getYear());
-		
+		//System.out.println(randomDate.getMonth()+" "+randomDate.getDayOfMonth()+" "+randomDate.getYear());
+
 		
 	}
-	*/
+	**/
+	
 }
