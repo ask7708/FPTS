9d898ccf01cff7610b4ed9c0d82d08273e77553d
Daniel Cypher <dcc7331@g.rit.edu>
2015-10-16 08:18:23 -0400

    Adding comments to a few files

--- src/Release1/ReadHoldingsContext.java
+++ src/Release1/ReadHoldingsContext.java
@@ -8,13 +8,24 @@ package Release1;
  *
  */
 public class ReadHoldingsContext {
-
-	   private ReadHoldingsStrategy strategy;
-
+	   private ReadHoldingsStrategy strategy; //Current Strategy being used
+	   /**
+	    * 
+	    * @param strategy - this is used so that the strategy is linked to the context
+	    * 
+	    *ReadHoldingsContext is used to set the corresponding Strategy so that the proper
+	    *holding will be created using the right arraylist
+	    */
 	   public ReadHoldingsContext(ReadHoldingsStrategy strategy){
-	      this.strategy = strategy;
+	      this.strategy = strategy; //sets the method equal to the  parameter
 	   }
-
+	   /**
+	    * 
+	    * @param temp - the array being used in the strategy
+	    * @return created holding due to the strategy
+	    * 
+	    * this executes the actual strategy and returns the corresponding holding
+	    */
 	   public Holdings executeStrategy(String[] temp){
 	      return strategy.readHolding(temp);
 	   }
--- src/Release1/ReadOwnedEquities.java
+++ src/Release1/ReadOwnedEquities.java
@@ -13,20 +13,26 @@ import java.time.format.DateTimeFormatter;
 public class ReadOwnedEquities extends ReadHoldingsStrategy {
 
 	@Override
+	/**
+	 * String[] temp -> the array of Strings that contian the parameters to create an owned Equity
+	 * 
+	 * creates an Equity object that has been made for a user (number of shares is there)
+	 * using an array list that contains its parameters
+	 */
 	public Holdings readHolding(String[] temp) {
-		String temp1 = temp[1];
-		String temp2 = temp[2];
-		double temp3 = Double.parseDouble(temp[3]);
-		double temp4 = Double.parseDouble(temp[4]);
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
-		LocalDate Time = LocalDate.parse(temp[5], formatter);
-		Equity EquityInfo = new Equity(temp1, temp2, temp3);
+		String temp1 = temp[1]; //ticker Symbol
+		String temp2 = temp[2]; //name
+		double temp3 = Double.parseDouble(temp[3]); //Number of shares
+		double temp4 = Double.parseDouble(temp[4]); //Price of Shares
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd"); //Type of date format
+		LocalDate Time = LocalDate.parse(temp[5], formatter); //time in the corresponding format
+		Equity EquityInfo = new Equity(temp1, temp2, temp3); //creating Equity
 		for(int x = 6; x < temp.length; x++){
-			EquityInfo.addIndexOrSec(temp[x]);
+			EquityInfo.addIndexOrSec(temp[x]); //adding the number of Sec according the length of the Equity
 		}
-		EquityInfo.setAcquiredShares(temp4);
-		EquityInfo.setDate(Time.toString());
-		return EquityInfo;
+		EquityInfo.setAcquiredShares(temp4); //Setting number of shares
+		EquityInfo.setDate(Time.toString()); //Setting the date 
+		return EquityInfo;  //returning Equity
 	}
 	
 	
