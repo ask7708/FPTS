12c25d8d430137e1b9247a5d97e8b00554026d8c
Daniel Roach <dannycr09@hotmail.com>
2015-10-15 23:09:14 -0400

    Made some changes Fought a giant merge conflict Life is hard

--- src/Release1/MarketAccount.java
+++ src/Release1/MarketAccount.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
  */
 public class MarketAccount extends Account {
 	
-	public MarketAccount(String accountName, double amount, LocalDate date, int accountNum, int routingNum) {
+	public MarketAccount(String accountName, double amount, String date, int accountNum, int routingNum) {
 		super(accountName, amount, date, accountNum, routingNum);
 		// TODO Auto-generated constructor stub
 	}
